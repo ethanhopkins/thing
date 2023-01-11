36e3e707b0d838c0e508c4e65f3694664f9061c4
ethanhopkins <112411800+ethanhopkins@users.noreply.github.com>
2023-01-11 13:20:36 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author jh270
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+	}
+
+}
